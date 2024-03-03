984bb21cf704d651fae6b6a157e82435028dda96
Kartik <kartik.mathur10@gmail.com>
2024-03-03 18:18:10 +0530

    Modules in nodeJS

---
+++ L19-NodeJS-Express/4_Destructure/app.js
@@ -0,0 +1,3 @@
+const {sum} = require('./library');
+
+console.log(sum);
---
+++ L19-NodeJS-Express/4_Destructure/script.js
@@ -0,0 +1,13 @@
+let obj = {
+    a: 1,
+    b: true,
+    c: "Hello"
+};
+
+
+// let {a} = obj;
+let {a,b,c} = obj;
+
+console.log(a)
+// console.log(b)
+// console.log(c)
