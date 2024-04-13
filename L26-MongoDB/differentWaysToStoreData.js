976a07b61b7100905654ca7fb7dfd8bc97c1ec25
Kartik <kartik.mathur10@gmail.com>
2024-04-13 18:30:48 +0530

    MongoDB

---
+++ L26-MongoDB/differentWaysToStoreData.js
@@ -0,0 +1,32 @@
+// To use new db students
+// use students;
+
+let students = [
+    {
+        name: "Badal",
+        address: "SRM university"
+    },
+    {
+        name: "Aayush",
+        address: "DTU"
+    },
+    {
+        name: "RUCHI",
+        address: "IP"
+    },
+    {
+        name: "Lakhan",
+        address: "IIT-D"
+    },
+    {
+        name: "Lakhan-I",
+        address: "SRM University"
+    },
+    {
+        name: "Lakhan-II",
+        address: "DTU"
+    }
+]
+
+// To insert data into the table
+// db.students.insertMany(students);
